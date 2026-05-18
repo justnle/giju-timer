713b0f0df04a0d95a4b627c7ad5fde0df99341c1
Justin <info@justinle.me>
2026-05-18 15:57:04 -0700

    next deployment

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'giju-timer';
+
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: { unoptimized: true },
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+};
+
+export default nextConfig;
