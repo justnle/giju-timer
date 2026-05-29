b3545be7921c871c7afc32f37984914509b1e73d
Justin <info@justinle.me>
2026-05-28 22:16:04 -0700

    update config for new domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'giju-timer';
-
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
 };
 
 export default nextConfig;
